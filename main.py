9c3e803a6c117b641c9777abb89aa6d6cb2b3102
dwarnecke <121268577+dwarnecke@users.noreply.github.com>
2024-04-09 14:33:09 -0500

    Add main.py
    Main file now trains a convolutional neural network and exports the model parameters to a json file.

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ Build a machine learning model to recognize handwritten digits.
 
 __author__ = 'Dylan Warnecke'
 
-__version__ = '1.0'
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-from activations import relu
-from layers import Dense, Dropout, InputLayer
-from losses.categorical_cross_entropy import CategoricalCrossEntropy
-from model import Model
-from optimizers.adam import Adam
-from utilities import make_one_hot
+from framework.activations import Relu, Tanh
+from framework.layers import Dense, Dropout, Input
+from framework.layers import Convolution, Flatten, MaxPool
+from framework.losses.categorical_cross_entropy import CategoricalCrossEntropy
+from framework.model import Model
+from framework.optimizers.schedules import Exponential
+from framework.optimizers import Adam
+from framework.utilities import make_one_hot
 
 
 if __name__ == '__main__':
@@ -24,61 +23,41 @@ if __name__ == '__main__':
     training_pixels = training_set.drop(columns='label').to_numpy()
     training_digits = training_set.loc[:, 'label'].to_numpy()
     training_digits = make_one_hot(training_digits, 10)
+    training_images = training_pixels.reshape((-1, 28, 28, 1))
 
-    # Make the neural network model
-    digit_model = Model(
+    # Make the dense network model
+    model = Model(
         CategoricalCrossEntropy(from_logits=True),
-        InputLayer((784,)),
-        Dense(128, relu),
-        Dropout(0.3),
-        Dense(128, relu),
-        Dropout(0.3),
+        Input((28, 28, 1)),
+        Convolution(16, 3, True, Relu()),
+        Dropout(0.1),
+        MaxPool(2),
+        Convolution(64, 3, True, Relu()),
+        Dropout(0.1),
+        MaxPool(2),
+        Flatten(),
+        Dense(256, Tanh()),
+        Dropout(0.5),
         Dense(10))
 
     # Fit the model using gradient descent
-    training_history = digit_model.fit(
-        training_pixels,
+    training_history = model.fit(
+        training_images,
         training_digits,
-        batch_size=256,
-        epochs=30,
-        alpha=1e-4,
-        optimizer=Adam(0.9, 0.99))
+        batch_size=2**9,
+        epochs=2**5,
+        optimizer=Adam(0.9, 0.99, Exponential(2e-4, 0.98)))
 
     # Plot the model training history
-    plt.plot(training_history)
-    plt.title("Training Model Loss Over Epochs")
-    plt.xlabel("Epoch")
-    plt.xscale('log')
-    plt.ylabel("Loss")
-    plt.yscale('log')
-    plt.show()
+    fig = plt.figure(figsize=(4, 4))
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    axes.plot(training_history)
+    axes.set_title("Training Model Loss Over Epochs")
+    axes.set_xlabel("Epoch")
+    axes.set_xscale('log')
+    axes.set_ylabel("Loss")
+    axes.set_yscale('log')
+    plt.show(block=True)
 
-    # Import and convert the testing data
-    testing_set = pd.read_csv('testing-digits.csv')
-    testing_set_size = testing_set.shape[0]
-    testing_pixels = testing_set.to_numpy()
-    testing_images = testing_pixels.reshape((testing_set_size, 28, 28))
-
-    # Shuffle the testing data examples
-    generator = np.random.default_rng()
-    shuffling_indices = generator.permutation(testing_set_size)
-    testing_pixels = testing_pixels[shuffling_indices]
-    testing_images = testing_images[shuffling_indices]
-
-    # Check if the user wants to evaluate model performance
-    keyboard_input = input("View example predictions: ")
-    if keyboard_input == 'y':
-        print("Model performance being evaluated...")
-
-        # Check the model performance over several examples
-        for digit_idx in range(7):
-            evaluation_digit = testing_images[digit_idx]
-            flattened_digit = np.ravel(evaluation_digit)
-            digit_prediction = digit_model.predict(flattened_digit[np.newaxis])
-            digit_prediction = digit_prediction.argmax(axis=1)[0]
-
-            # Message the predicted digit and show it
-            print(f"Predicted number: {digit_prediction}", end='')
-            plt.imshow(evaluation_digit)
-            plt.show()
-            print("\r", end='')
+    # Save the model for later importation
+    digit_model.save('model.json')
